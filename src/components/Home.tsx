2670f0e85ae447ad465c11921e280e1747aaaab9
admin <k.gerlovin@gmail.com>
2021-02-11 01:05:14 -0500

    misc meta

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -20,32 +20,35 @@ class Home extends Component<IHomeProps, IHomeState> {
     render () {
         return (
             <div>
-                <nav className="bg-gray-800 ">
-                    <div className="max-w-7xl mx-auto px-2 py-2 sm:px-6 lg:px-8 sm:flex sm:justify-start">
-                        <div className="whitespace-nowrap text-center sm:text-left">
-                            <h1 className="text-white pr-0 pb-2 sm:pb-0 sm:mr-8 text-l sm:text-2xl font-bold sm:pr-4 cursor-default tracking-tighter ">
-                                interviewPrep
+                <div className="w-full">
+
+                    <nav className="bg-gray-800 ">
+                        <div className="max-w-7xl mx-auto px-2 py-2 sm:px-6 lg:px-8 sm:flex sm:justify-start">
+                            <div className="whitespace-nowrap text-center sm:text-left">
+                                <h1 className="text-white pr-0 pb-2 sm:pb-0 sm:mr-8 text-l sm:text-2xl font-bold sm:pr-4 cursor-default tracking-tighter ">
+                                    interviewPrep
+                                </h1>
+                            </div>
+                            <div className="flex space-x-4 flex-wrap items-center justify-center sm:items-stretch sm:justify-start">
+                                <a href="#" className={this._getTabClassName(Category.DOTNET)} onClick={() => {this._selectTab(Category.DOTNET)}}>{CategoryName.get(Category.DOTNET)}</a>
+                                <a href="#" className={this._getTabClassName(Category.ANGULAR)} onClick={() => {this._selectTab(Category.ANGULAR)}}>{CategoryName.get(Category.ANGULAR)}</a>
+                                <a href="#" className={this._getTabClassName(Category.JSWEBDOM)} onClick={() => {this._selectTab(Category.JSWEBDOM)}}>{CategoryName.get(Category.JSWEBDOM)}</a>
+                                <a href="#" className={this._getTabClassName(Category.SQL)} onClick={() => {this._selectTab(Category.SQL)}}>{CategoryName.get(Category.SQL)}</a>
+                                <a href="#" className={this._getTabClassName(Category.GENERAL)} onClick={() => {this._selectTab(Category.GENERAL)}}>{CategoryName.get(Category.GENERAL)}</a>
+                            </div>
+                        </div>
+
+                    </nav>
+
+                    <header className="bg-white shadow">
+                            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                            <h1 className="text-l font-bold leading-tight text-gray-900">
+                                {CategoryDescription.get(this.state.categoryId)}
                             </h1>
                         </div>
-                        <div className="flex space-x-4 flex-wrap items-center justify-center sm:items-stretch sm:justify-start">
-                            <a href="#" className={this._getTabClassName(Category.DOTNET)} onClick={() => {this._selectTab(Category.DOTNET)}}>{CategoryName.get(Category.DOTNET)}</a>
-                            <a href="#" className={this._getTabClassName(Category.ANGULAR)} onClick={() => {this._selectTab(Category.ANGULAR)}}>{CategoryName.get(Category.ANGULAR)}</a>
-                            <a href="#" className={this._getTabClassName(Category.JSWEBDOM)} onClick={() => {this._selectTab(Category.JSWEBDOM)}}>{CategoryName.get(Category.JSWEBDOM)}</a>
-                            <a href="#" className={this._getTabClassName(Category.SQL)} onClick={() => {this._selectTab(Category.SQL)}}>{CategoryName.get(Category.SQL)}</a>
-                            <a href="#" className={this._getTabClassName(Category.GENERAL)} onClick={() => {this._selectTab(Category.GENERAL)}}>{CategoryName.get(Category.GENERAL)}</a>
-                        </div>
-                    </div>
-
-                </nav>
-
-                <header className="bg-white shadow">
-                        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        <h1 className="text-l font-bold leading-tight text-gray-900">
-                            {CategoryDescription.get(this.state.categoryId)}
-                        </h1>
-                    </div>
-                </header>
-                <main>
+                    </header>
+                </div>
+                <main className="">
                     <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                     <div className="px-4 py-6 sm:px-0">
                         <TopicPage categoryId={this.state.categoryId}></TopicPage>
